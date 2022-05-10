6034a171ad9351cbe83d2a5f0f705eb45a9048af
SamvelBarseghyan <sam.barseghyan.96@gmail.com>
2022-05-11 01:57:39 +0400

    Update entrypoint of API

--- src/app.py
+++ src/app.py
@@ -1,12 +1,30 @@
-import uvicorn
-from fastapi import FastAPI
+import io
+import yaml
+import functools
+from controller.controller import router
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import Response
 
 
-def create_app() -> FastAPI:
-    app = FastAPI()
-    return app
+app = FastAPI(
+    title="ETL Project",
+    description="API provides functionality to calculate KPIs of "
+                "Dota player",
+    version="0.1"
+)
+app.include_router(router)
+
+
+@app.get('/openapi.yaml', include_in_schema=False)
+@functools.lru_cache()
+def read_openapi_yaml() -> Response:
+    """ Exports yaml openapi definition"""
+    openapi_json = app.openapi()
+    yaml_s = io.StringIO()
+    yaml.dump(openapi_json, yaml_s)
+    return Response(yaml_s.getvalue(), media_type='text/yaml')
 
 
 if __name__ == '__main__':
-    curr_app = create_app()
-    uvicorn.run(curr_app)
+    import uvicorn
+    uvicorn.run(app)
