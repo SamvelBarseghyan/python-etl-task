c525658a894f0701c8502fec65a76ad420e4a163
SamvelBarseghyan <sam.barseghyan.96@gmail.com>
2022-05-13 02:51:48 +0400

    Add unit tests for RecentMatches

--- tests/test_entity_recent_matches.py
+++ tests/test_entity_recent_matches.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 from src.entity.matches import RecentMatches, Match
 
 
+
 class TestRecentMatches(unittest.TestCase):
     def setUp(self) -> None:
         self.account_id = 318351912
@@ -50,21 +51,51 @@ class TestRecentMatches(unittest.TestCase):
                 }
             ]
         }
-        self.match_obj_1 = Match(self.match_info_1, self.account_id)
-        self.match_obj_2 = Match(self.match_info_2, self.account_id)
 
     def test_init(self):
-        ...
-        # matches_list = [self.match_info_1, self.match_info_2]
-        # recent_matches_obj = RecentMatches(matches_list, self.account_id)
-        # match_obj_list = [
-        #     self.match_obj_1,
-        #     self.match_obj_2
-        # ]
-        # self.assertEqual(match_obj_list, recent_matches_obj.recent_matches)
-        # self.assertEqual()
-        # self.assertEqual()
-        # self.assertEqual()
-        # self.assertEqual()
-        # self.assertEqual()
-        # self.assertEqual()
+        matches_list = [self.match_info_1, self.match_info_2]
+        recent_matches_obj = RecentMatches(matches_list, self.account_id)
+
+        self.assertEqual(4.14, recent_matches_obj.min_kda)
+        self.assertEqual(7.4, recent_matches_obj.avg_kda)
+        self.assertEqual(10.67, recent_matches_obj.max_kda)
+        self.assertEqual("241.67%", recent_matches_obj.min_kp)
+        self.assertEqual("243.91%", recent_matches_obj.avg_kp)
+        self.assertEqual("246.15%", recent_matches_obj.max_kp)
+
+    def test_get_min_avg_max_of_list_kda(self):
+        list_of_nums = [4.4, 1, 14, 0.5, 1.23]
+        expected_tuple = 0.5, 4.23, 14
+
+        @dataclass()
+        class MockClass:
+            kda: float
+
+        matches_list = [self.match_info_1, self.match_info_2]
+        recent_matches_obj = RecentMatches(matches_list, self.account_id)
+
+        recent_matches_obj.recent_matches = list()
+        for num in list_of_nums:
+            recent_matches_obj.recent_matches.append(MockClass(num))
+        self.assertEqual(
+            expected_tuple,
+            recent_matches_obj.get_min_avg_max_of_list(True)
+        )
+
+    def test_get_min_avg_max_of_list_kp(self):
+        list_of_nums = [4.4, 1, 14, 0.5, 1.23]
+        expected_tuple = "0.5%", "4.23%", "14%"
+
+        @dataclass()
+        class MockClass:
+            kp: float
+
+        matches_list = [self.match_info_1, self.match_info_2]
+        recent_matches_obj = RecentMatches(matches_list, self.account_id)
+        recent_matches_obj.recent_matches = list()
+        for num in list_of_nums:
+            recent_matches_obj.recent_matches.append(MockClass(num))
+        self.assertEqual(
+            expected_tuple,
+            recent_matches_obj.get_min_avg_max_of_list(False)
+        )
